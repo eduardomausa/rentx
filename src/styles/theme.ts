cff5493eaa4b175ab603b15e75eb14e24079937c
Eduardo Mausa <mausaeduardo@gmail.com>
2022-10-16 20:13:36 -0300

    calendar fix

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,29 +1,29 @@
 export default {
   colors: {
-    header: '#1B1B1F',
+    header: '#1b1b1f',
 
-    background_primary: '#F4F5F6',
-    background_secondary: '#FFFFFF',
+    background_primary: '#f4f5f6',
+    background_secondary: '#fff',
 
-    text: '#7A7A80',
-    text_detail: '#AEAED3',
-    title: '#47474D',
+    text: '#7a7a80',
+    text_detail: '#aeaeb3',
+    title: '#47474d',
 
-    line: '#EBEBF0',
+    line: '#ebebf0',
 
-    main: '#DC1637',
-    main_light: '#FDEDEF',
+    main: '#dc1637',
+    main_light: '#fdedef',
 
-    success: '#03B252',
+    success: '#03b252',
 
-    shape: '#E1E1E8',
-    shape_dark: '#29292E',
+    shape: '#e1e1e8',
+    shape_bold: '#29292e',
   },
 
   fonts: {
     primary_400: 'Inter_400Regular',
     primary_500: 'Inter_500Medium',
-  
+
     secondary_400: 'Archivo_400Regular',
     secondary_500: 'Archivo_500Medium',
     secondary_600: 'Archivo_600SemiBold',
--- src/utils/getPlataformDate.ts
+++ src/utils/getPlataformDate.ts
@@ -1,5 +1,11 @@
 import { addDays } from 'date-fns';
+import { Platform } from 'react-native';
 
-export function getPlatformDate(date: Date) {
-  return addDays(date, 1);
+export function getPlatformDate(date: Date): Date {
+
+  if (Platform.OS === 'ios') {
+    return addDays(date, 1)
+  } else {
+    return date;
+  }
 }
